fba2c123ac192aef466abcb6a5d3bc9703ec60dd
sukhchain-mansa <developer@mansainfotech.com>
2020-02-17 16:00:47 +0530

    Added file check system. It will detect if the first argument provided is a file or not.

--- File-Packer.c
+++ File-Packer.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "pack.h"
 #include "unpack.h"
 
 int main(int argc, char *argv[]) {
 
     if (argc > 1) {
-        for (char i = 0; i < argc; i++)
+        if ( access( argv[1], F_OK ) != -1 ) {
+            printf("%s will be used to load files and directories \
+for Packing and Unpacking.\n", argv[1]);
+        } else { 
+            printf("%s is not a file.\n", argv[1]);
+        }
+
+        for (char i = 2; i < argc; i++)
         {
             printf("%s\t", argv[i]);
         }
