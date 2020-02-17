89c1d52d9369361bf9a1e8d2d7cd8a1108797d92
mansa-de <developer@mansainfotech.com>
2020-02-17 14:55:03 +0530

    In this branch I implemented the command-line arguments parser feature.

--- File-Packer.c
+++ File-Packer.c
@@ -2,8 +2,19 @@
 #include "pack.h"
 #include "unpack.h"
 
-int main() {
+int main(int argc, char *argv[]) {
 
+    if (argc > 1) {
+        for (char i = 0; i < argc; i++)
+        {
+            printf("%s\t", argv[i]);
+        }
+        printf("\n");
+    }
+    else
+    {
+        printf("You didn't provide any arguments.\n");
+    }
     
 
     return 0;
