60bad179e79183ff8f9658d563d322528c54d9a7
sukhchain-mansa <developer@mansainfotech.com>
2020-02-17 16:14:04 +0530

    Added comments to the File-Packer.c file.

--- File-Packer.c
+++ File-Packer.c
@@ -5,7 +5,9 @@
 
 int main(int argc, char *argv[]) {
 
+    // Checks if arguments are provided or not.
     if (argc > 1) {
+        // Checks if file exists or not.
         if ( access( argv[1], F_OK ) != -1 ) {
             printf("%s will be used to load files and directories \
 for Packing and Unpacking.\n", argv[1]);
@@ -13,17 +15,18 @@ for Packing and Unpacking.\n", argv[1]);
             printf("%s is not a file.\n", argv[1]);
         }
 
+        // Prints the options provided to the program.
         for (char i = 2; i < argc; i++)
         {
             printf("%s\t", argv[i]);
         }
         printf("\n");
     }
+    // Print this if arguments weren't provided.
     else
     {
         printf("You didn't provide any arguments.\n");
     }
     
-
     return 0;
 }
