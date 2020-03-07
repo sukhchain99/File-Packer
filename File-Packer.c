e3d36b216fae8bb58dca34a1a42c1abebffd04f5
sukhchainn <ssaulakh2@gmail.com>
2020-03-07 21:30:08 +0530

    added option count

--- File-Packer.c
+++ File-Packer.c
@@ -8,7 +8,7 @@
 #define VERSION "0.0.0.1"
 
 // Options available globally
-char p_option[10], s_option[4];
+char OPTIONS[10], FEATURES[4];
 
 int main(int argc, char *argv[]) {
     printf("file-pack version : %s\n", VERSION);
@@ -18,77 +18,124 @@ int main(int argc, char *argv[]) {
         // Checks if file exists or not.
         if ( access( argv[1], F_OK ) != -1 ) {
             printf("\nusing %s\n", argv[1]);
-        } else {
+        } else { 
             printf("\n%s does not exist or not readable\n", argv[1]);
             exit(EXIT_FAILURE);
         }
 
-        // will contain the options provided through arguments
-        // options start from 2nd argument
-        enum Options enable[argc-2];
-        char buffer[10];
-
-        // Convert arguments to string buffer
-        for (char i = 2; i < argc; i++)
+        // OPTIONS count block
+        char options_count = 0;
         {
-            strcpy(buffer, argv[i]);
-            enable[i-2] = buffer[1];
-        }
-
-        // Enable the options provided to the program.
-        for (char i = 2; i < argc; i++)
-        {
-            switch (enable[i-2])
+            char temp[10];
+            for (int i = 2; i < argc; ++i)
             {
-            case 'P':
-                p_option[P] = 1;
-                break;
-            case 'o':
-                p_option[o] = 1;
-                break;
-            case 'r':
-                p_option[r] = 1;
-                break;
-            case 't':
-                p_option[t] = 1;
-                break;
-            case 'U':
-                p_option[U] = 1;
-                break;
-            case 'g':
-                p_option[g] = 1;
-                break;
-            case 'u':
-                p_option[u] = 1;
-                break;
-            case 'E':
-                p_option[E] = 1;
-                break;
-            case 's':
-                p_option[s] = 1;
-                break;
-            case 'e':
-                p_option[e] = 1;
-                break;
-
-            default:
-                printf("Wrong Option %s", argv[i]);
-                break;
+                strcpy(temp, argv[i]);
+                if (temp[0] == '-' && temp[1] != '-') {
+                    options_count++;
+                } else if (temp[0] == '-' && temp[1] == '-') {
+                    break;
+                } else {
+                    printf("Invalid option : %s\n", argv[i]);
+                    exit(EXIT_FAILURE);
+                }
             }
         }
-        printf("\n");
+
+        // FEATURES count block
+        char features_count = 0;
+        {
+            char temp[10];
+            for (int i = options_count+2; i < argc; ++i)
+            {
+                strcpy(temp, argv[i]);
+                if (temp[0] == '-' && temp[1] == '-') {
+                    features_count++;
+                } else {
+                    printf("Invalid feature : %s\n", argv[i]);
+                    exit(EXIT_FAILURE);
+                }
+            }
+        }
     }
 
     // Print this if arguments weren't provided.
     else
     {
         printf("You didn't provide any arguments.\n");
+        printf("Finding \"packer.fp\" file\n");
+
+        if ( access( "packer.fp", F_OK ) != -1 ) {
+            printf("\nusing \"packer.fp\"\n");
+        } else {
+            printf("\npacker.fp does not exist or not readable\n");
+            exit(EXIT_FAILURE);
+        }
+
     }
+
+    // will contain the options provided through arguments
+    // options start from 2nd argument
+    enum Options enable[argc-2];
+    char buffer[10];
+
+    // Convert arguments to string buffer
+    for (char i = 2; i < argc; i++)
+    {
+        strcpy(buffer, argv[i]);
+        enable[i-2] = buffer[1];
+    }
+
+    // Enable the options provided to the program.
+    for (char i = 2; i < argc; i++)
+    {
+        switch (enable[i-2])
+        {
+        case 'P':
+            OPTIONS[P] = 1;
+            break;
+        case 'o':
+            OPTIONS[o] = 1;
+            break;
+        case 'r':
+            OPTIONS[r] = 1;
+            break;
+        case 't':
+            OPTIONS[t] = 1;
+            break;
+        case 'U':
+            OPTIONS[U] = 1;
+            break;
+        case 'g':
+            OPTIONS[g] = 1;
+            break;
+        case 'u':
+            OPTIONS[u] = 1;
+            break;
+        case 'E':
+            OPTIONS[E] = 1;
+            break;
+        case 's':
+            OPTIONS[s] = 1;
+            break;
+        case 'e':
+            OPTIONS[e] = 1;
+            break;
+
+        default:
+            printf("Wrong Option %s", argv[i]);
+            break;
+        }
+    }
+    printf("\n");
 
     // To be removed after the implementation of pack.c and unpack.c
     for (char i = 0; i < 10; i++)
     {
-        printf("%d ", p_option[i]);
+        printf("%d ", OPTIONS[i]);
+    } printf(" ");
+    for (char i = 0; i < 4; i++)
+    {
+        printf("%d ", FEATURES[i]);
     } printf("\n");
 
     return 0;
